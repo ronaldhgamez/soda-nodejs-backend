f66098624693380602d8280164d5ca88674635c0
AliciaD2018 <aliciadiazrivas@gmail.com>
2021-06-03 17:29:33 -0600

    sodas update

---
+++ controllers/soda.js
@@ -0,0 +1,17 @@
+/* Variable's connection to Firebase */
+const db = require('../firebase/firebase_config');
+
+const modifySodas = async(req,res)=>
+{
+    try
+    {
+
+    }catch(error)
+    {
+
+    }
+}
+
+module.exports={
+    modifySodas
+}
