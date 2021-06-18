a460323caa1560c01759afdfc0bb89a5905ef17b
Jose Fonseca <joselfonseca97@gmail.com>
2021-06-18 11:36:22 -0600

    funciones para login,registro y perfil de usuario

--- controllers/customers.js
+++ controllers/customers.js
@@ -31,12 +31,106 @@ async function getSodas(req, res) {
         })
         res.status(200).send({ 'sodas': allCafes })
     } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+async function getClientData(req, res) {
+    console.log("getblientdata")
+    console.log(req.body.idUser)
+    try {
+        const clientsRef = db.collection('client');
+        const snapshot = await clientsRef.where('idCliente', '==', parseInt(req.body.idUser)).get();
+
+        /* validates if the username already exists */
+        if (snapshot.empty) {
+            return res.status(404).send({ msg: false })
+        }
+        else {
+            snapshot.forEach(doc => {
+                console.log(doc.data())
+                return res.status(200).send({ msg: doc.data() })
+            })
+        }
+    } catch (error) {
         console.log(error)
         return res.status(500).send(error)
     }
 }
 
+async function getClientPhone(req, res) {
+    try {
+        const clientsRef = db.collection('clientPhoneNumber');
+        const snapshot = await clientsRef.where('idCliente', '==', parseInt(req.body.idUser)).get();
+
+        /* validates if the username already exists */
+        if (snapshot.empty) {
+            console.log("no ha encontrado el telefono")
+            return res.status(404).send({ msg: false })
+        }
+        else {
+            console.log("Se encontró el telefono")
+            snapshot.forEach(doc => {
+                return res.status(200).send({ msg: doc.data() })
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error)
+    }
+}
+async function updateUserInfo(req, res) {
+    try {
+        const clientsRef = db.collection('client');
+        const snapshot = await clientsRef.where('idCliente', '==', parseInt(req.body.userId)).get();
+        let idDoc = null
+        snapshot.forEach(a => {
+            idDoc = a.id
+        })
+        const object = {
+            "nombre": req.body.nombre,
+            "apellidos": req.body.apellidos,
+            "contrasena": req.body.contrasena,
+            "direccionExacta": req.body.direccionExacta,
+            "idCliente": req.body.userId,
+            "idDistrito": req.body.idDistrito,
+            "imagenPerfil": req.body.imagenPerfil,
+            "usuario": req.body.usuario
+        }
+        console.log(idDoc)
+        console.log(req.body)
+        if (idDoc) {
+            await db.collection('client').doc(idDoc).update(object)
+        } else {
+            return res.status(500).send({ msg: false })
+        }
+
+        //actualizar telefono
+        const PhoneRef = db.collection('clientPhoneNumber');
+        const snapshotPhone = await PhoneRef.where('idCliente', '==', parseInt(req.body.userId)).get();
+        let idDocPhone = null
+        snapshotPhone.forEach(b => {
+            idDocPhone = b.id
+        })
+        const objectPhone = {
+            "idCliente": req.body.userId,
+            "telefono": req.body.telefono
+        }
+        if (PhoneRef) {
+            await db.collection('clientPhoneNumber').doc(idDocPhone).update(objectPhone)
+        } else {
+            return res.status(500).send({ msg: false })
+        }
+        return res.status(200).send({ msg: true })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error)
+    }
+}
 module.exports = {
     addCustomer,
-    getSodas
+    getSodas,
+    getClientData,
+    getClientPhone,
+    updateUserInfo
 }
